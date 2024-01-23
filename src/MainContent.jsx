64d20104fd38811c3422bc5ba42cca41cd5b21c9
Darren Song <dazzasong@gmail.com>
2024-01-24 09:46:06 +1100

    Made button icons change on click

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,20 +1,27 @@
 import { Box, Button, Stack } from "@mui/material";
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import CancelIcon from '@mui/icons-material/Cancel';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import React from 'react';
 
 export default function MainContent() {
   const [mode, setMode] = React.useState(0); // 0 = Initial, 1 = InGame, 2 = EndGame
   let buttonText
   let buttonColor
+  let startButtonIcon
+  let endButtonIcon
   if (mode === 0) {
     buttonText = "Start new game"
     buttonColor = "green"
+    startButtonIcon = <LocalFireDepartmentIcon />
   } else if (mode === 1) {
     buttonText = "End game"
     buttonColor = "red"
+    endButtonIcon = <CancelIcon />
   } else {
     buttonText = "Rematch"
     buttonColor = "orange"
+    endButtonIcon = <RefreshIcon />
   }
   function clickGameButton() {
     if (mode === 0) setMode(1)
@@ -27,7 +34,8 @@ export default function MainContent() {
         <Button disableRipple disableElevation
           onClick={clickGameButton}
           variant="contained"
-          startIcon={<LocalFireDepartmentIcon />}
+          startIcon={startButtonIcon}
+          endIcon={endButtonIcon}
           sx={{
             fontSize: 20,
             fontWeight: "bold",
